aba1cb3fb84c65ad44fbd06e961618cd3b875bc9
DariaHolovko <holovko2003@gmail.com>
2022-10-02 14:57:28 -0400

    showing date/time last updated

--- src/index.js
+++ src/index.js
@@ -9,10 +9,34 @@ function displayTemp(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let feelingElement = document.querySelector("#feeling");
+  let dateElement = document.querySelector("#date");
   cityElement.innerHTML = response.data.name;
   tempElement.innerHTML = `${Math.round(response.data.main.temp)}˚`;
   weatherDescriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = response.data.wind.speed;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
   feelingElement.innerHTML = Math.round(response.data.main.feels_like);
+  dateElement.innerHTML = getDate(response.data.dt * 1000);
+}
+function getDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
